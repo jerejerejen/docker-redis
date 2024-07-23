b9d88c75c7711ef638cb1ff8df3b0d96fb57b0e5
jerejerejen <158231535+jerejerejen@users.noreply.github.com>
2024-07-23 13:42:32 +0900

    predixy 프록시 서버 제거, 비밀번호 설정, 백업 설정(aof, 스냅샷), volatile-lru

--- cluster.sh
+++ cluster.sh
@@ -1,6 +1,7 @@
-echo redis-server /etc/redis/redis_6300.conf
+o redis-server /etc/redis/redis_6300.conf
 redis-cli --cluster create redis-node1:6300 redis-node2:6301 redis-node3:6302 --cluster-yes
 redis-cli --cluster add-node redis-node4:6400 redis-node1:6300 --cluster-slave --cluster-yes
 redis-cli --cluster add-node redis-node5:6401 redis-node2:6301 --cluster-slave --cluster-yes
 redis-cli --cluster add-node redis-node6:6402 redis-node3:6302 --cluster-slave --cluster-yes
 echo success
+
